b0b67ec3c4737029c1c481a6ec4c8429fc97ee3d
Paul Perez <colopaul@hotmail.com>
2019-12-04 21:56:23 -0500

    step 1 and 2 are done

---
+++ redux-thunk/src/components/Notes.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import fetchUser from "../index.js";
+console.log(fetchUser);
+const Notes = () => {
+	// console.log(fetchUser().then((data) => console.log(data)));
+	return <div>testing</div>;
+};
+
+export default Notes;
